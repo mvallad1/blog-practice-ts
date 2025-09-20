4dba07a104f224646052fc9d3c0633282b17aad5
Moses Valladares <mvallad1@binghamton.edu>
2025-09-19 23:35:36 -0400

    started blog section and context for usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
+import { IoMdAddCircle } from "react-icons/io"
 import Navigation from "./components/Navigation"
 import PeopleToFollow from "./components/PeopleToFollow"
 import TopicsList from "./components/TopicsList"
 import TrendsList from "./components/TrendsList"
+import { BlogProvider } from "./share/BlogContext"
 
 const App = () => {
   return (
     <div>
-      <Navigation />
+      <BlogProvider>
+        <Navigation />
 
-      <div className="flex justify-center">
-        {/* main area*/}
+        <div className="flex justify-center">
+          {/* main area*/}
+          <section className="mx-auto p-6">
+            <div>
+              <button onClick={openModelForNewBlog} className="ml-[7rem] bg-black flex justify-center items-center text-white px-4 py-2 rounded mb-4">
+                Add New Blog <IoMdAddCircle className="ml-[0.5rem]"/>
+              </button>
+            </div>
+          </section>
 
-        <div className="w-[30%]">
-          <PeopleToFollow />
-          <TrendsList />
-          <TopicsList />
+          <div className="w-[30%]">
+            <PeopleToFollow />
+            <TrendsList />
+            <TopicsList />
+          </div>
         </div>
-      </div>
 
+      </BlogProvider>
     </div>
   )
 }
